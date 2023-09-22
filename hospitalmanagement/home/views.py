8384e9df01ea8c42c96130016d230144ed501fbf
Nandhukriss <103727372+Nandhukriss@users.noreply.github.com>
2023-09-23 02:52:27 +0530

    initial commit

---
+++ hospitalmanagement/home/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def index(request):
+    return 
+
+def about(request):
+    return 
+
+def booking(request):
+    return 
+
+def doctors(request):
+    return 
+
+def contact(request):
+    return 
